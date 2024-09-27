8937770e747964c019b163b3e698f9542b0b2114
Aleksey Serduik <aleksey.serduyk@gmail.com>
2024-09-27 12:27:01 +0600

    added StorageServices

--- Navigator/Profile/ProfileViewController.swift
+++ Navigator/Profile/ProfileViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import StorageServices
 
 class ProfileViewController: UIViewController {
     
--- StorageServices/Post.swift
+++ StorageServices/Post.swift
@@ -5,19 +5,23 @@
 //  Created by Алексей Сердюк on 18.09.2024.
 //
 
-struct PostTitle {
-    var title: String
+public struct PostTitle {
+    public var title: String
+    
+    public init(title: String) {
+        self.title = title
+    }
 }
 
-struct Post {
-    let author: String
-    let description: String
-    let image: String
-    let likes: Int
-    let views: Int
+public struct Post {
+    public let author: String
+    public let description: String
+    public let image: String
+    public let likes: Int
+    public let views: Int
 }
 
-let posts: [Post] = [
+public let posts: [Post] = [
     Post(author: "Autor 1", description: "Lorem Ipsum - это текст-\"рыба\", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной \"рыбой\" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.", image: "ios18", likes: 123, views: 123),
     Post(author: "Autor 2", description: "Lorem Ipsum - это текст-\"рыба\", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной \"рыбой\" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.", image: "ios18", likes: 2222, views: 233),
     Post(author: "Autor 3", description: "Lorem Ipsum - это текст-\"рыба\", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной \"рыбой\" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.", image: "ios18", likes: 3123, views: 323),
